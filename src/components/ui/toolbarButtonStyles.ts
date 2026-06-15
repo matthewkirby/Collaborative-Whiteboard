623b7665abc22d5729e2a2eef52d09aa7c2892a1
Xopar <xoparr@gmail.com>
2026-06-15 13:58:50 -0500

    Fixing tiny itty bitty styling bug

--- src/components/ui/toolbarButtonStyles.ts
+++ src/components/ui/toolbarButtonStyles.ts
@@ -7,4 +7,4 @@ export const toolbarButtonActive =
   "underline bg-blue-900 text-blue-100 border-blue-500 hover:bg-blue-800";
 
 export const toolbarActionButton =
-  "p-2 border-r border-y border-gray-600 flex items-center justify-center text-gray-100 bg-gray-700 hover:bg-gray-600 active:bg-blue-900 active:text-blue-100 transition-colors";
+  "p-2 first:border-l border-r border-y border-gray-600 flex items-center justify-center text-gray-100 bg-gray-700 hover:bg-gray-600 active:bg-blue-900 active:text-blue-100 transition-colors";
